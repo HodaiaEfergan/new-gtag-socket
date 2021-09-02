1eeba548ff0bf985372e973140f0e3a719bf24bb
HodaiaEfergan <62573380+HodaiaEfergan@users.noreply.github.com>
2021-09-02 12:20:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -40,7 +40,8 @@ let socketServer = net.createServer(function (socket) {
                 console.log('sample came in');
                 let uid = str.substring('UID'.length, data.indexOf(' Send'));
                 console.log(uid);
-                
+                console.log(uid.getConfiguration());
+                socket.write(uid.getConfiguration());
                 axios.get(serverUrl + 'sample?data=' + data);
                 return;
             }
